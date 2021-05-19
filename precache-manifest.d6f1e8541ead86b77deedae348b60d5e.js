0cab2dd2eaabb6d6ebc833c9491cab63f50a21a2
itachi0 <>
2021-05-19 22:12:11 +0800

    Updates

---
+++ precache-manifest.d6f1e8541ead86b77deedae348b60d5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efe455153984eea792bf13e5cc5ce47e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49c75befca94ca9cdf21",
+    "url": "/static/css/main.de9d4c8d.chunk.css"
+  },
+  {
+    "revision": "030f90b654793292e3a3",
+    "url": "/static/js/2.9fffa4da.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.9fffa4da.chunk.js.LICENSE"
+  },
+  {
+    "revision": "49c75befca94ca9cdf21",
+    "url": "/static/js/main.ce1429d9.chunk.js"
+  },
+  {
+    "revision": "628c421b313c877df073",
+    "url": "/static/js/runtime-main.62f5e097.js"
+  }
+]);
